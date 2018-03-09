b321287d15790949145d7f6b2dc71122f58b2f12
Larry Botha <larry@fixate.it>
2018-03-09 08:43:14 +0100

    add annotations for different Jest modes

--- src/sum.js
+++ src/sum.js
@@ -1,3 +1,15 @@
+// This function is now a dependency in get-adder. If an error is introduced
+// here, Jest will use a dependency tree to run all fiels that have the dependency
 const sum = (a, b) => a + b;
+
+// By default Jest in watch mode will compare changed files to git's HEAD.
+// This can be changed to the last commit by running:
+// $ jest --watch --lastCommit
+
+// Jest can be put in a run-all mode by pressing 'a' when active. 'o' will change
+// Jest back to running tests only on files that have changed.
+
+// Pattern mode can be entered by pressing 'p'. Files matching a pattern will only
+// be watched, e.g. 'p', then 'sum' will only watch files that contain 'sum'
 
 export default sum;
